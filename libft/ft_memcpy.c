e2bb68cf9e07e8cc3a4243c672af89af75ba58b6
Ruben Munoz Calderon <rmunoz-c@c1r15s3.42madrid.com>
2024-10-01 17:27:07 +0200

    Correcciones2

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rmunoz-c <rmunoz-c@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 14:44:39 by rmunoz-c          #+#    #+#             */
-/*   Updated: 2024/10/01 16:54:36 by rmunoz-c         ###   ########.fr       */
+/*   Updated: 2024/10/01 17:25:38 by rmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*ptr;
 	const unsigned char	*ptr2;
 
+	if (!dest && !src)
+		return (NULL);
 	count = 0;
 	ptr = (unsigned char *)dest;
 	ptr2 = (const unsigned char *)src;
@@ -29,7 +31,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
-/*int	main(void)
+int	main(void)
 {
 	char *src = NULL;
 	char *dest = NULL;
@@ -38,4 +40,4 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	
 	write(1, src, 19);
 	return (0);
-}*/
+}
