c1bb774e785fa0c76f80aed7a98a428b7d8713fa
Ruben Munoz Calderon <rmunoz-c@c3r14s3.42madrid.com>
2024-10-30 20:49:40 +0100

    Arreglo del Makefile

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rmunoz-c <rmunoz-c@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 18:04:23 by rmunoz-c          #+#    #+#             */
-/*   Updated: 2024/10/30 17:16:33 by rmunoz-c         ###   ########.fr       */
+/*   Updated: 2024/10/30 20:48:48 by rmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int		ft_putchar(int c);
 int		ft_putui(unsigned int n);
 int		ft_put_hex_ptr(unsigned long long int n, const char format, int is_ptr);
 int		count_digits(unsigned int n);
-char		*ft_utoa(unsigned int n);
+char	*ft_utoa(unsigned int n);
 int		ft_printf(char const *s, ...);
 int		ft_conver(va_list args, const char format);
 
